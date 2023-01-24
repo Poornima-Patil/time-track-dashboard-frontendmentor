91c2e12edf66a5a317d81814e928ec6e332f6a30
Poornima-Patil <linkinpoornima@gmail.com>
2023-01-24 08:01:09 +0530

    Completed commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                orange: '#ff8a63',
+                bluebg: '#1e204b',
+                playbg: '#56c2e6',
+                studybg: '#ff5e7d',
+                exercisebg: '#4bcf83',
+                socialbg: '#7235d1',
+                selfcarebg: '#f0c75a',
+            },
+        },
+    },
+    plugins: [],
+}
